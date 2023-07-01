5936013d6c08a04a8415b2aa82f87f73e8ebef60
lchetanlc <81949498+lchetanlc@users.noreply.github.com>
2023-07-02 05:26:56 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -131,7 +131,7 @@ import logos from './logos.png'
       <div class="grid place-items-center bg-gradient-to-r from-teal-500 to-teal-600">
         <h1 class="text-white text-4xl font-poppins  font-bold leading-normal mt-5">Backed By The Best</h1>
         <p class="text-white text-1xl font-poppins font-normal  leading-normal">Wealthup is backed and supported by</p>
-        <Image src={first} height={150}></Image>
+        <Image src={first} height={150} alt="sponsors"></Image>
       </div>
       <div class="bg-blue-900 pt-5 ">
         <h1 class="text-white text-4xl font-poppins font-extrabold text-center relative top-[20px]">Transforming Lives - Financially!</h1>
@@ -144,18 +144,18 @@ import logos from './logos.png'
               <div>Simrin Sirur</div>
               <div>Journalist</div>
               <div class="text-sm dark:text-gray-400">The Print, Gurugram</div>
-              <div class="w-[450px] text-l relative top-[10px]">"I was afraid to learn about investments and savings because it seemed to fly over my head, but Ankit was extremely patient and walked me through everything... I now feel more confident to handle my own money."</div>
+              <div class="w-[450px] text-l relative top-[10px]">`"I was afraid to learn about investments and savings because it seemed to fly over my head, but Ankit was extremely patient and walked me through everything... I now feel more confident to handle my own money."`</div>
             </div>
           </figcaption>
-          <Image class="rounded-full w-6 h-7" src={icon} alt="profile picture" />
-          <Image class="w-150 h-100 " src={one} alt="profile picture" />
+          <Image class="rounded-full w-6 h-7" src={icon} alt="arrow" />
+          <Image class="w-150 h-100 " src={one} alt="grid-picture" />
         </div>
 
 
 
       </div>
       <div className="flex justify-center bg-gradient-to-b from-[#1FB3A5] to-[#18A1A7] ">
-        <Image className='object-cover h-35 w-98' src={logos} />
+        <Image className='object-cover h-35 w-98' src={logos} alt="Logos"/>
       </div>
     </div>
   )
